0f1b07b328087caf7edec2b7ddb3f5149b4570eb
scare-destiny <zhenya.venger@gmail.com>
2023-11-04 19:05:16 +0200

    pusgh

--- app/page.tsx
+++ app/page.tsx
@@ -118,25 +118,27 @@ export default function Home() {
 				<div className=' pb-32 sm:pb-48 lg:pb-48 '>
 					<div className='relative isolate overflow-hidden pt-14'>
 						<img
-							src='/hero.png'
+							src='/image3.jpeg'
 							alt=''
-							className='absolute inset-0 -z-10 h-full w-full contrast-[.70]  object-cover'
+							className='absolute inset-0 -z-10 h-full w-full object-cover'
 						/>
 						<div
-							className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+							className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden  sm:-top-80'
 							aria-hidden='true'
 						>
-							<div
+							{/* <div
 								className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
 								style={{
 									clipPath:
 										'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
 								}}
-							/>
+							/> */}
 						</div>
+
 						<div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
 							<div className='hidden sm:mb-8 sm:flex sm:justify-center'></div>
-							<div className='text-center'>
+
+							<div className=' text-center z-50'>
 								<FadeIn>
 									<h1 className='text-4xl font-bold font-serif tracking-tight text-white sm:text-6xl'>
 										Підписка на шкарпетки
@@ -147,10 +149,11 @@ export default function Home() {
 									Отримуй улюблені шкарпетки поштою. Швидко і регулярно. Запуск зовсім
 									скоро!
 								</p>
+
 								<div className='mt-10 flex items-center justify-center gap-x-6'>
 									<a
 										href='#'
-										className='rounded-md bg-amber-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400'
+										className='rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400'
 									>
 										Зареєструватись
 									</a>
