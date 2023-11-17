16e7a956caea33c1805fdb62ce6a19d46323e964
scare-destiny <zhenya.venger@gmail.com>
2023-11-17 13:26:10 +0200

    removed button

--- components/CTA.1.tsx
+++ components/CTA.1.tsx
@@ -56,12 +56,12 @@ export function CTA({ scrollToPricing }) {
 						та прев&apos;ю нових продуктів!
 					</p>
 					<div className='mt-8'>
-						<button
+						{/* <button
 							onClick={scrollToPricing}
 							className='inline-flex  rounded-md bg-custom-blue px-3.5 py-2.5   text-sm font-semibold  shadow-sm hover:bg-white/20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
 						>
 							Обрати Підписку
-						</button>
+						</button> */}
 					</div>
 				</div>
 			</div>
